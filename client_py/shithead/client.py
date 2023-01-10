0383a07415a40e673d67dcc30694cbd02789c3f7
Johannes Gutekunst <gutekunst@dive-solutions.de>
2023-01-10 22:27:53 +0100

    client project configuration

--- client_py/shithead/client.py
+++ client_py/shithead/client.py
@@ -4,24 +4,24 @@ import json
 import websockets
 
 
-async def hello():
-    join_game_id = "1"
+async def main():
+    join_game_id = 1
     uri = f"ws://localhost:8000/game/{join_game_id}"
     async with websockets.connect(uri) as websocket:
         response = await websocket.recv()
         print(f"<<< {response}")
         print("server request ...")
-        await websocket.send(json.dumps({"type": "START"}))
 
-        print(f"<<< WAITING FOR GAME TO BEGIN")
         response = await websocket.recv()
-        dict = json.loads(response)
-        if dict["type"] == "start":
-            print("client kicks off first move")
-
+        print(f"<<< {response}")
+        await websocket.send(json.dumps({"type": "start_game"}))
+        response = await websocket.recv()
         print(f"<<< {response}")
         response = await websocket.recv()
+        print(f"<<< {response}")
+        response = await websocket.recv()
+        print(f"<<< {response}")
 
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    asyncio.run(main())
